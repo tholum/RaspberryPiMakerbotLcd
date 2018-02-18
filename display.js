6fc8558274bae833fcac6dc95d5919c8592bcff6
tholum <tholum@couleetechlink.com>
2018-02-18 13:12:33 -0600

    Display Class

--- display.js
+++ display.js
@@ -1,5 +1,23 @@
 const http = require("http");
 
 module.exports = function( params ){
-
+    const five = params.five;
+    var lcd = new five.LcdShift(
+		{
+			register,
+			pins : [ 1 , 3 , 4 , 5 ,6 , 7],
+					rows: 4,
+					cols: 20
+		}
+    );
+    this.write = function( col , row , len , str ){
+        lcd.clear();
+        str = str.substr( 0 , len );
+        while( str.length < len ){
+            str += " ";
+        }
+        lcd.cursor( col , row ).print( str );
+    }
+  
+   
 }
--- panel.js
+++ panel.js
@@ -23,19 +23,30 @@ let getJSON = (url , cb ) => {
 	      console.log("Got an error: ", e);
 	});
 }
-var ctof = ( c ) => {
-	return (c * 9/5) + 32;
-}
+
 var five = require("johnny-five");
 five.LcdShift = require("./lcd-shift");
-var Raspi = require("raspi-io");
-var board = new five.Board({
+const Display = require("./display");
+const Raspi = require("raspi-io");
+const board = new five.Board({
   io: new Raspi()
 });
 
 board.on("ready", function() {
-  var led = new five.Led(ledPin);
-  led.blink();
+	
+	const led = new five.Led(ledPin);
+	const display = new Display( { five });
+	display.write( col , row , len , str );
+	led.blink();
+	let i = 0;
+	setInterval( () => {
+		getJSON( `http://localhost/api/printer?apikey=${APIKEY}` , ( res ) => {
+			i++;
+			display.write( 0 , 0 , 10 , `B: ${res.temperature.bed.actual}` );
+			display.write( 0 , 10 , 10 , `E: ${res.temperature.tool0.actual}` );
+			display.write( 1 , 0 , 10 , `${res.state.text}` );
+			display.write( 2 , 0 , 10 , `${i}` );
+		} , 1000  );
   var register = new five.ShiftRegister({
     pins: {
       data,
@@ -43,20 +54,6 @@ board.on("ready", function() {
       latch
     }
   });
-  var lcd = new five.LcdShift(
-	{
-		register,
-		pins : [ 1 , 3 , 4 , 5 ,6 , 7],
-		    rows: 4,
-		    cols: 20
-	}
-  );
-   lcd.clear();
-   setInterval( () => {
-   getJSON( `http://localhost/api/printer?apikey=${APIKEY}` , ( res ) => {
-	lcd.cursor( 0 , 0 ).print(`TEST B: ${res.temperature.bed.actual}`)
-	.cursor( 0 , 10 ).print(`E: ${res.temperature.tool0.actual}`)
-        .cursor( 1 , 0 ).print( `${res.state.text}`);
-   } );
-   } , 1000  );
+  
+   
 });
