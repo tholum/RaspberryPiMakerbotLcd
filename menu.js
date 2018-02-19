9d6e8fb0f3aeda3922584a8014aae41628291de1
tholum <tholum@couleetechlink.com>
2018-02-19 17:22:07 -0600

    Refactor : Blacker Boxes

--- menu.js
+++ menu.js
@@ -93,7 +93,6 @@ module.exports = function( params ){
             this.unselectMenu();
         }
     });
-    console.log(buttons);
     buttons.centerButton.on("down" , () => {
         this.eval("center");
     });
