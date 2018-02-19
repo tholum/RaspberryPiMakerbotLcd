fb5bb775a93b9653bb18a97a7a6e00e50fe6ced2
tholum <tholum@couleetechlink.com>
2018-02-18 21:36:59 -0600

    Job Status

--- menus/status.js
+++ menus/status.js
@@ -26,9 +26,21 @@ module.exports = function(params){
         clearInterval(interval);
         let updateStatus = () => {
             getJSON( `http://localhost/api/printer?apikey=${config.apikey}` , ( res ) => {
-                display.write( 0 , 0 , 10 , `B: ${res.temperature.bed.actual}` );
-                display.write( 0 , 10 , 10 , `E: ${res.temperature.tool0.actual}` );
+                display.write( 0 , 0 , 10 , `B:${Math.round(res.temperature.bed.actual)}/${Math.round(res.temperature.bed.target)}` );
+                display.write( 0 , 10 , 10 , `E:${Math.round(res.temperature.tool0.actual)}/${Math.round(res.temperature.tool0.target)}` );
                 display.write( 1 , 0 , 20 , `Status: ${res.state.text}` );
+            });
+            getJSON( `http://localhost/api/job?apikey=${config.apikey}` , ( res ) => {
+                let filename = "";
+                let progress = "";
+                if( res.hasOwnProperty("job") && res.job.hasOwnProperty("file")){
+                    filename = res.job.file.name;
+                }
+                if( res.hasOwnProperty("progress") && res.progress.hasOwnProperty("completion") ){
+                    progress = `${Math.round( res.progress.completion )}%`;
+                }
+                display.write( 2 , 0 , 10, `P: ${progress}` );
+                display.write( 3 , 0 , 5, `${filename}` );
             });
         };
         updateStatus();
