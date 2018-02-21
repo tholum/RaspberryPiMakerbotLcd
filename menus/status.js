26feaec15fdb36c4340a38446fce7b0c73325727
tholum <tholum@couleetechlink.com>
2018-02-21 18:42:38 +0000

    Commands

--- menus/status.js
+++ menus/status.js
@@ -9,7 +9,12 @@ let getJSON = (url , cb ) => {
 	    });
 
 	    res.on('end', function(){
-	        cb( JSON.parse(body) );
+		console.log( body );
+		try {
+			cb( JSON.parse(body) );
+		} catch( error ){
+			cb( {error : body } );
+		}
 	    });
 	}).on('error', function(e){
 	      console.log("Got an error: ", e);
@@ -25,12 +30,20 @@ module.exports = function(params){
         clearInterval(interval);
         let updateStatus = () => {
             getJSON( `${config.octoprintUrl}/api/printer?apikey=${config.apikey}` , ( res ) => {
+		    if( res.hasOwnProperty("error") ){
+		    	display.write( 0 , 0 , 20 , res.error );
+			return;
+		    }
                 display.write( 0 , 0 , 10 , `B:${Math.round(res.temperature.bed.actual)}/${Math.round(res.temperature.bed.target)}` );
                 display.write( 0 , 10 , 10 , `E:${Math.round(res.temperature.tool0.actual)}/${Math.round(res.temperature.tool0.target)}` );
                 display.write( 1 , 0 , 20 , `Status: ${res.state.text}` );
             });
             getJSON( `${config.octoprintUrl}/api/job?apikey=${config.apikey}` , ( res ) => {
-                let filename = "";
+		    if( res.hasOwnProperty("error") ){
+		    	display.write( 3 , 0 , 20 , res.error );
+			return;
+		    }
+ 		 let filename = "";
                 let progress = "";
                 if( res.hasOwnProperty("job") && res.job.hasOwnProperty("file")){
                     filename = res.job.file.name;
